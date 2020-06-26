218c76eab0dcbcad4f6a7e8094cc275d9c9f5c9e
Maikon Canuto <maikoncanuto@gmail.com>
2020-06-26 19:52:39 -0300

    Configuracao do microservice-cliente, discovery e gateway

---
+++ mcp-discovery/src/main/java/com/github/maikoncanuto/discovery/McpDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.github.maikoncanuto.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class McpDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpDiscoveryApplication.class, args);
+    }
+
+}
